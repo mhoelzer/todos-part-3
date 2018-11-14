fc0b928f94654f96cb7c54fcb972d742a303dec8
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-14 13:36:14 -0500

    finsihed cirerstia?

--- todos/src/App.js
+++ todos/src/App.js
@@ -3,47 +3,55 @@ import logo from './logo.svg';
 import './App.css';
 import todoList from './todos.json';
 
-class Todo extends Component {
+class TodoItem extends Component {
   state = {
-    text: String,
-    checked: false
-  }
+    title: String,
+    completed: true
+  };
   render() {
     return (
-
-    )
-  }
-}
+      <li className={this.props.completed && "completed"}>
+        <div className="view">
+          <input className="toggle" type="checkbox" checked={this.props.completed}/>
+          <label>{this.props.title}</label>
+          <button className="destroy"></button>
+        </div>
+      </li>
+    );
+  };
+};
 
 class TodoList extends Component {
+  state = {
+    todos: todoList
+  };
   render() {
     return (
-      
-    )
-  }
-}
+      <section className="main">
+        <ul className="todo-list">
+          {this.state.todos.map(todo => <TodoItem key={todo.id} title={todo.title} completed={todo.completed} />)}
+        </ul>
+      </section>
+    );
+  };
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input className="new-todo" placeholder="What needs to be done?" autofocus />
         </header>
-      </div>
+        <TodoList/>
+        <footer className="footer">
+          <span className="todo-count"><strong>0</strong> item(s) left</span>
+          <button className="clear-completed">Clear completed</button>
+        </footer>
+      </section>
     );
-  }
-}
+  };
+};
 
 export default App;
