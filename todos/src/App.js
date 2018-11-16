0fa443b9c7b0bece9f3172df44819492671e0ef7
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-15 23:02:40 -0500

    got checked stuff to work, but that's about it

--- todos/src/App.js
+++ todos/src/App.js
@@ -8,17 +8,18 @@ import todoList from './todos.json';
 class TodoItem extends Component {
   render() {
     // if dont do this, change this to match the stuff with todos and this.state
-    const { title, completed, destroyOneTodo } = this.props;
+    const { title, completed, handleToggleCompletedTodo, handleDestroyOne } = this.props;
     return (
       // it will be completed
       // <li className={completed && "completed"}> // this will make an error
       // still want something to return even if empty
       <li className={completed ? "completed" : ""}>
         <div className="view">
-          <input className="toggle" type="checkbox" defaultChecked={completed} onClick={completed}/>
+          {/* pass handlers down to children to w/e comp has html tag */}
+          <input className="toggle" type="checkbox" defaultChecked={completed} onClick={handleToggleCompletedTodo}/>
           {/* change checked to defaultChecked b/c that's not a react thingy */}
           <label>{title}</label>
-          <button className="destroy" onClick={destroyOneTodo}></button>
+          <button className="destroy" onClick={handleDestroyOne}></button>
         </div>
       </li>
     );
@@ -26,31 +27,85 @@ class TodoItem extends Component {
 };
 
 class TodoList extends Component {
+  state = {
+    todos: todoList
+  };
+
+  // function inside funciton; once called the first time, the first outer funxtion will run and implicitly returns inner function (the event handler); first param is event obj sent from dom; cTI can be accessed inside inner funct
+  handleToggleCompletedTodo = clickedTodoId => event => {
+    // map makes new array to get results in newtodos
+    const newTodos = this.state.todos.map(todo => {
+      // get todo id that matches one was clicked; if this is false, no run
+      if (todo.id === clickedTodoId) { 
+        // change completed value/any boolean value (t vs f)
+        todo.completed = !todo.completed; 
+      } 
+      return todo; // have to return original todo obj
+    });
+    // overwrite todos w/ newtodos
+    this.setState({todos: newTodos});
+  };
+
+  handleDestroyOne = clickedTodoId => event => {
+    const newTodos = this.state.todos.map(todo => {
+      if (todo.id === clickedTodoId) {
+        todo.completed = !todo.completed;
+        // return false
+      }
+      return todo;
+      // return true;
+    });
+    this.setState({todos: newTodos});
+  };
+
   render() {
     // if didnt do this, change todos.map to this.state.todos.map; do use state but not modifying it now
-    // const { todos } = this.state;
+    const { todos } = this.state;
     return (
       <section className="main">
-        <ul className="todo-list">{this.props.children}</ul>
+        <ul className="todo-list">
+          {/* when put in comp state; when inside the map function, it will be a single todo */}
+          {todos.map(todo => <TodoItem key={todo.id} title={todo.title} completed={todo.completed} handleToggleCompletedTodo={this.handleToggleCompletedTodo(todo.id)}/>)}
+          {/* call the hTCT and pass in the todoid and store inside clickedTodoId var, which causes cTI to be in scope for event function  --> completeTodo can also just be hTCT; it rerenders only 1 when props change */}
+        </ul>
       </section>
     );
   };
 };
 
 class App extends Component {
-  state = {todos: todoList}
-  // {todos.map(todo => <TodoItem key={todo.id} title={todo.title} completed={todo.completed} />)}
-  handleMarkCompletedTodo = idUserClicked => event => {
-    const { todos } = this.state;
-    const newTodos = todos.map(todo => {
-      if(todo.id === idUserClicked) {
-        todo.completed = true;
+  state = {
+    todos: todoList
+  };
+
+  handleAddTodo = event => {
+    const {todos} = this.state;
+    let makeId = Math.floor(Math.random() * 333666999);
+    if(event.keyCode === 13) { // 13 is the code for enter; could also do event.key == "Enter"
+      let newlyEnteredTodo = {
+        userId: 1,
+        id: makeId,
+        title: event.target.value,
+        completed: false
+      };
+      todos.push(newlyEnteredTodo);
+      this.setState({todos: todos});
+      event.target.value = "";
+    };
+  };
+
+  handleDeleteAllCompletedTodos = event => {
+    const newTodos = this.state.todos.filter(todo => {
+      if (todo.completed === true) {
+        return false;
       }
-      return todo;
+      return true;
     });
-    this.setState({todos: newTodos});
-    // <TodoItem key={todo.id} title={todo.title} completed={todo.completed} />
-  }
+    this.setState({
+      todos: newTodos
+    });
+  };
+  
   render() {
     return (
       <section className="todoapp">
@@ -62,7 +117,7 @@ class App extends Component {
         <TodoList />
         <footer className="footer">
           <span className="todo-count"><strong>0</strong> item(s) left</span>
-          <button className="clear-completed">Clear completed</button>
+          <button className="clear-completed" onClick={this.handleDeleteAllCompletedTodos}>Clear completed</button>
         </footer>
       </section>
     );
