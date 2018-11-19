2ff5d29b4eeed0794d2ea929d807f84b0c474ac5
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-19 09:42:10 -0500

    Updates

---
+++ precache-manifest.498e754ce7cae7a98a0e2f17a6581cfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a440ae0955513ca116f4",
+    "url": "/todos-part-2/static/css/main.10b8ac2d.chunk.css"
+  },
+  {
+    "revision": "a440ae0955513ca116f4",
+    "url": "/todos-part-2/static/js/main.a440ae09.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/todos-part-2/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "56578612cadf6faa79ed",
+    "url": "/todos-part-2/static/js/runtime~main.56578612.js"
+  },
+  {
+    "revision": "b0bae7a8efbcf7b9d9a4eaf36871f944",
+    "url": "/todos-part-2/index.html"
+  }
+];
