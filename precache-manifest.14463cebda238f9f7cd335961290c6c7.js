6687814cfa1d94e7ad77ebdccae00d9b13f1c036
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-27 10:05:54 -0500

    Updates

---
+++ precache-manifest.14463cebda238f9f7cd335961290c6c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc9d8b5e9aa77f5e14f1",
+    "url": "/todos-part-3/static/css/main.fe9fc5cf.chunk.css"
+  },
+  {
+    "revision": "dc9d8b5e9aa77f5e14f1",
+    "url": "/todos-part-3/static/js/main.dc9d8b5e.chunk.js"
+  },
+  {
+    "revision": "726455bb2cf63a39bec6",
+    "url": "/todos-part-3/static/js/1.726455bb.chunk.js"
+  },
+  {
+    "revision": "eb5d57de38284f0a3d03",
+    "url": "/todos-part-3/static/js/runtime~main.eb5d57de.js"
+  },
+  {
+    "revision": "44e89c20dcdf14962e17e47341349358",
+    "url": "/todos-part-3/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todos-part-3/precache-manifest.3f06c95967af214368949f5809009024.js"
+  "/todos-part-3/precache-manifest.14463cebda238f9f7cd335961290c6c7.js"
 );
 
 workbox.clientsClaim();
